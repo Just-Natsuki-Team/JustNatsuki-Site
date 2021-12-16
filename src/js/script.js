74bda8ae1184060b39de47d8d33d21cf17ab2798
multimokia <multimokia3@gmail.com>
2021-12-15 19:08:51 -0500

    snow during d25 season

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 import "@babel/polyfill";
 import 'whatwg-fetch';
 import "./closest";
+import "./snow";
+import startSnow from "./snow";
 
 window.javascriptLocales = {
 	copyMessage: "Just wanted to let you know that Satania is always with you! Even inside your clipboard! We are everywhere and you should really join us!\n" +
@@ -18,7 +20,6 @@ window.javascriptLocales = {
 /*
 	SLIDESHOW
 */
-
 for (const slideshow of document.getElementsByClassName("slideshow")) {
 	const slides = slideshow.querySelectorAll("picture");
 	let currentSlide = 0;
@@ -26,9 +27,18 @@ for (const slideshow of document.getElementsByClassName("slideshow")) {
 		slides[currentSlide].classList.remove("shown");
 		currentSlide = (currentSlide + 1) % slides.length;
 		slides[currentSlide].classList.add("shown");
-	}, 2500);
+	}, 5000);
 }
 
 document.getElementById('close-language-protip').addEventListener("click", () => {
 	document.getElementById('language-protip').style.display = 'none';
 });
+
+//Snow is automatically enabled when between december 11th and january 1st
+const CURR_YEAR = new Date().getFullYear();
+const D25_START = new Date(CURR_YEAR, 11, 11);
+const D25_END = new Date(CURR_YEAR+1, 0, 1);
+
+if (D25_START.getTime() <= new Date().getTime() && new Date().getTime() <= D25_END.getTime()) {
+    startSnow();
+}
